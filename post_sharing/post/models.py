a8916474cfa1999413c250c64f677250b850e648
keshav.mahajan <keshav.mahajan@netsolutions.com>
2019-07-19 17:37:37 +0530

    description filed added in the model

--- post_sharing/post/models.py
+++ post_sharing/post/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=255, default='')
