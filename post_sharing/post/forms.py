d62b38fdb61481937fd43520453687420eeaa217
keshav.mahajan <keshav.mahajan@netsolutions.com>
2019-07-19 19:19:39 +0530

    added some descriptive information in database

--- post_sharing/post/forms.py
+++ post_sharing/post/forms.py
@@ -7,3 +7,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'image', 'description']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols':15})
+        }
